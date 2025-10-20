2c5c8fa44b75ee684b7efe747fb4e18354bd5b7b
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-10-20 23:15:29 +0700

    Add new mothod getCarByBrandsAndYears

--- Main.java
+++ Main.java
@@ -9,9 +9,15 @@ public class Main{
         Car[] Toyota = getCarByBrend(cars, "Toyota");
         Car[] Honda = getCarByBrend(cars, "Honda");
         Car[] BNW = getCarByBrend(cars, "BNW");
+        Car[] Ytoyota = getCarByBrendAndYearOperational(cars, "Toyota", 4);
+        Car[] Yhonda = getCarByBrendAndYearOperational(cars, "Honda", 5);
+        Car[] YBNW = getCarByBrendAndYearOperational(cars, "BNW", 5);
         System.out.println("Кол-во Тойот: " + Toyota.length);
         System.out.println("Кол-во Hond: " + Honda.length);
         System.out.println("Кол-во BNW: " + BNW.length);
+        System.out.println("Кол-во Тойот: " + Ytoyota.length);
+        System.out.println("Кол-во Hond: " + Yhonda.length);
+        System.out.println("Кол-во BNW: " + YBNW.length);
     }
     public static Car[] getCarByBrend(Car[] cars, String brend){
         int c = 0;
@@ -26,12 +32,33 @@ public class Main{
         Car[] res = new Car[c];
         int ind = 0;
         for(int i = 0; i < cars.length; i++){
-            if(cars[i].getBrand() == brend){
+            if(cars[i].getBrand().equals(brend)){
                 res[ind] = cars[i];
                 ind++;
             }
         }
         return res;
     }
-
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years){
+        int c = 0;
+        for(int i=0; i < cars.length; i++){
+            int expl_year = 2025 - cars[i].getYear();
+            if(cars[i].getBrand() == brend && expl_year > years){
+                c++;
+            }
+        }
+        if(c == 0){
+            return new Car[0];
+        }
+        Car[] result = new Car[c];
+        int ind = 0;
+        for(int i = 0; i < cars.length; i++){
+            int explYear = 2025 - cars[i].getYear();
+            if(cars[i].getBrand().equals(brend) && explYear < years){
+                result[ind] = cars[i];
+                ind++;
+            }
+        }
+        return result; 
+    }
 }

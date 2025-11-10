a554636ec83af441f3e131ab343abc4c12e027c3
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-11-10 21:05:39 +0700

    Добавлен метод add

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
 public class Cache<T>{
     private int n;
-    public Cache(n){
+    private Object List[];
+    private int size;
+    public Cache(int n){
         if(n <= 0){
-            throw new Exception("Количество элементов в кэше должно быть больше 0");
+            throw new IllegalArgumentException("Количество элементов в кэше должно быть больше 0");
         }
         this.n = n;
+        this.List = new Object[n];
+        this.size = 0;
     }
-    
+    public void add(T item){
+        if (item == null){
+            throw new IllegalArgumentException("Не чувак пустой элемент не будет добавлен");
+        }
+        
+        if(size<n){
+            List[size] = item;
+            size++;
+        }else{
+            for(int i = 0; i < n-1; i++){
+                List[i] = List[i+1];
+            }
+            List[n-1] = item;
+        }
+    }
 }
 
 

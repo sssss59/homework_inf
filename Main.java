e04f3f477a7b9a8a1cd1f2405163998a52fd795b
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-11-10 20:18:27 +0700

    Создан Cache<T>

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
+public class Cache<T>{
+    private int n;
+    public Cache(n){
+        if(n <= 0){
+            throw new Exception("Количество элементов в кэше должно быть больше 0");
+        }
+        this.n = n;
+    }
+    
+}
+
+
 public class Main{
     public static void main(String[] args){
 

aeaa6ef82b049d2ef4868a8e6f9fb5010e197f0c
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-11-10 21:50:02 +0700

    New method getFirst

--- Main.java
+++ Main.java
@@ -51,6 +51,10 @@ public class Cache<T>{
         }
         return false;
     }
+    public T getFirst(){
+        if (size > 0 && List[0] != null){return (T) List[0];}
+        else{return null;}
+    }
     
 }
 

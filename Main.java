82891ca0a161eccd285b5fdca5638d4aec9f5689
Симачев Артур ИП-513 <maim.shaulong@gmail.com>
2025-11-17 18:18:24 +0700

    Добавлен конструктор в Cache

--- Main.java
+++ Main.java
@@ -2,7 +2,18 @@ import java.util;
 
 public class Cache<T>{
     private final LinkedList <T> array;
-    private final size;
+    private int size;
 
-    
+    public Cache(int size){
+        if(size <= 0){
+            throw new IllegalArgumentException("Размер должен быть больше 0");
+        }else{
+            this.size = size;
+            this.array = new LinkedList<T>();
+        }
+    }
+
+    void add(T item){
+        
+    }
 }
